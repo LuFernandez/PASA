c40cfac63e00d9d96998838b81b33c491fbdc3c2
taomasgonzalez <togonzalez@itba.edu.ar>
2019-09-08 13:40:46 -0300

    cambios bobos en main

--- TP1. Laboratorio de estimación no-paramétrica/OndaGravitacional/main.py
+++ TP1. Laboratorio de estimación no-paramétrica/OndaGravitacional/main.py
@@ -1,7 +1,19 @@
+import numpy as np
+from scipy import signal as scsig
 
-#hago blackman-tukey
-#out = np.correlate(a=strain_l1[0:len(strain_l1)//3], v=strain_l1[0:len(strain_l1)//3], mode="full")
-#out = out[len(out)//2:]
-plt.acorr(strain_l1)
-scsig.windows.get_window("boxcar", len(strain_l1), fftbins=True)
-plt.show()
+# hago blackman-tukey
+# out = np.correlate(a=strain_l1[0:len(strain_l1)//3], v=strain_l1[0:len(strain_l1)//3], mode="full")
+# out = out[len(out)//2:]
+# plt.acorr(strain_l1)
+# scsig.windows.get_window("boxcar", len(strain_l1), fftbins=True)
+# plt.show()
+
+# nperseg : L
+
+
+
+fs = nfft = nperseg = 512
+t = np.arange(8192) / fs
+x = np.sin(2*np.pi*50*t) + np.sin(2*np.pi*100*t) + np.sin(2*np.pi*150*t)
+
+welch_barlett(data=x, fs=fs, title='', D=2, nperseg=512, nfft=512)
