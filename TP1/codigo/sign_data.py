1221f412323dc034eece3c56d871b4ad532f2868
LuFernandez <lu.gfernandez@gmail.com>
2019-11-03 19:19:10 -0300

    calculé J para los de signo

--- TP1/codigo/sign_data.py
+++ TP1/codigo/sign_data.py
@@ -3,13 +3,13 @@ import numpy as np
 
 def sign_data_filter(u, d, w0, mu, N):
     k = len(w0)
-    w = sign_data(u, d, w0, mu, N)
+    w, J = sign_data(u, d, w0, mu, N)
     y = []
     u_ = np.concatenate((np.zeros(k), u))
     for i in range(0, len(w)):
         y.append(np.dot(w[i], u_[i:i+k]))
 
-    return y
+    return y, J
 
 
 def sign_data(u, d, w0, mu, N):
@@ -20,13 +20,14 @@ def sign_data(u, d, w0, mu, N):
     for i in range(0, k):
         w.append(w0)
     for i in range(k, m):
-        w.append(sign_data_step(u[i-k:i], d[i], w[i-1], mu, N))
+        w_i, J = sign_data_step(u[i - k:i], d[i], w[i - 1], mu, N)
+        w.append(w_i)
 
-    return w
+    return w, J
 
 
 def sign_data_step(u, d, w0, mu, N):
-
+    J = []
     w = w0  # add initial condition to output so we can iterate
     # u2 = np.dot(u, u)
     for _ in range(N):
@@ -34,8 +35,9 @@ def sign_data_step(u, d, w0, mu, N):
         e = d-y
         delta_w = mu * np.sign(u) * e
         w = w + delta_w
+        J.append(e * e)
 
-    return w
+    return w, J
 
 
 
--- TP1/codigo/sign_sign.py
+++ TP1/codigo/sign_sign.py
@@ -3,13 +3,13 @@ import numpy as np
 
 def sign_sign_filter(u, d, w0, mu, N):
     k = len(w0)
-    w = sign_sign(u, d, w0, mu, N)
+    w, J = sign_sign(u, d, w0, mu, N)
     y = []
     u_ = np.concatenate((np.zeros(k), u))
     for i in range(0, len(w)):
         y.append(np.dot(w[i], u_[i:i+k]))
 
-    return y
+    return y, J
 
 
 def sign_sign(u, d, w0, mu, N):
@@ -20,13 +20,14 @@ def sign_sign(u, d, w0, mu, N):
     for i in range(0, k):
         w.append(w0)
     for i in range(k, m):
-        w.append(sign_sign_step(u[i-k:i], d[i], w[i-1], mu, N))
+        w_i, J = sign_sign_step(u[i - k:i], d[i], w[i - 1], mu, N)
+        w.append(w_i)
 
-    return w
+    return w, J
 
 
 def sign_sign_step(u, d, w0, mu, N):
-
+    J = []
     w = w0  # add initial condition to output so we can iterate
     # u2 = np.dot(u, u)
     for _ in range(N):
@@ -34,8 +35,9 @@ def sign_sign_step(u, d, w0, mu, N):
         e = d-y
         delta_w = mu * np.sign(u) * np.sign(e)
         w = w + delta_w
+        J.append(e * e)
 
-    return w
+    return w, J
 
 
 
