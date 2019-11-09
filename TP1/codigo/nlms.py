087a74ffa459d4fee26fe3c2ffd173592ab724fe
rocioparra <rparra@itba.edu.ar>
2019-11-09 15:00:34 -0300

    nlms working, montecarlo

--- TP1/codigo/nlms.py
+++ TP1/codigo/nlms.py
@@ -1,31 +1,30 @@
 import numpy as np
 
 
-def nlms(u, d, w0, mu, N):
+def nlms(u, d, w0, mu):
     k = len(w0)
     m = len(u)
-    w = []
+    w = [w0]
 
-    for i in range(0, k):
-        w.append(w0)
-    for i in range(k, m):
-        w_i, J = nlms_step(u[i-k:i], d[i], w[i-1], mu, N)
+    u_ = np.concatenate((np.zeros(k-1), u))
+    for i in range(m):
+        w_i, J = nlms_step(u_[i:i+k], d[i], w[i-1], mu)
         w.append(w_i)
 
+    del w[0]
     return w, J
 
 
-def nlms_step(u, d, w0, mu, N):
+def nlms_step(u, d, w0, mu):
     J = []
     w = w0  # add initial condition to output so we can iterate
     u2 = np.dot(u, u)
     muu = mu*u/(u2+0.00001)
-    for _ in range(N):
-        y = np.dot(w, u)
-        e = d-y
-        delta_w = muu * e
-        w = w + delta_w
-        J.append(e * e)
+    y = np.dot(w, u)
+    e = d-y
+    delta_w = muu * e
+    w = w + delta_w
+    J.append(e * e)
 
     return w, J
 
